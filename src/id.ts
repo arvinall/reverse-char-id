167447df086b1ff74e75f3b7ec3045d6834dec9e
Arvinall <arvinall021@gmail.com>
2021-09-29 14:47:01 +0330

    feat: Implement Reverse Char ID

---
+++ src/id.ts
@@ -0,0 +1,92 @@
+import { CodeRange } from '.'
+
+const { String } = globalThis
+
+export function getFirstCodeRangesChar (codeRanges: CodeRange[]): string {
+  return String.fromCodePoint(codeRanges[0][0])
+}
+
+export function getLastCodeRangesChar (codeRanges: CodeRange[]): string {
+  return String.fromCodePoint(codeRanges[codeRanges.length - 1][1])
+}
+
+export function codeRangeIsReverse (codeRange: CodeRange): boolean {
+  return codeRange[1] < codeRange[0]
+}
+
+export function getCharCodeRange (
+  char: string,
+  codeRanges: CodeRange[]
+): CodeRange | undefined {
+  const charCode: number = char.codePointAt(0) as number
+
+  for (const codeRange of codeRanges) {
+    if (
+      !codeRangeIsReverse(codeRange) &&
+      charCode >= codeRange[0] &&
+      charCode <= codeRange[1]
+    ) return codeRange
+
+    if (
+      charCode >= codeRange[1] &&
+      charCode <= codeRange[0]
+    ) return codeRange
+  }
+}
+
+export function getNextCodeRangesChar (
+  char: string,
+  codeRanges: CodeRange[]
+): string {
+  const charCode: number = char.codePointAt(0) as number
+  const codeRange: CodeRange = getCharCodeRange(char, codeRanges) as CodeRange
+
+  let nextCode: number = charCode
+
+  if (charCode === codeRange[1]) {
+    nextCode = codeRanges[codeRanges.indexOf(codeRange) + 1][0]
+  } else {
+    if (!codeRangeIsReverse(codeRange)) nextCode++
+    else nextCode--
+  }
+
+  return String.fromCodePoint(nextCode)
+}
+
+export function generateNextID ({
+  codeRanges,
+  lastID
+}: {
+  codeRanges: CodeRange[]
+  lastID: string
+}): string {
+  let newID: string = ''
+
+  if (lastID.length === 0) {
+    newID = getFirstCodeRangesChar(codeRanges)
+  } else {
+    let mustAddNewChar: boolean = true
+    let newIDIsReady: boolean = false
+
+    for (const char of lastID.split('').reverse()) {
+      let newChar: string = char
+
+      if (!newIDIsReady) {
+        if (char === getLastCodeRangesChar(codeRanges)) {
+          newChar = getFirstCodeRangesChar(codeRanges)
+        } else {
+          newChar = getNextCodeRangesChar(char, codeRanges)
+
+          mustAddNewChar = false
+          newIDIsReady = true
+        }
+      }
+
+      newID = newChar + newID
+    }
+
+    if (mustAddNewChar) newID += getFirstCodeRangesChar(codeRanges)
+  }
+
+  return newID
+}
---
+++ src/index.ts
@@ -0,0 +1,95 @@
+import { generateNextID } from './id'
+
+const {
+  String,
+  Number,
+  Array,
+  Symbol
+} = globalThis
+
+export type CharRange = [string, string]
+export type CodeRange = [number, number]
+export type Range = CharRange | CodeRange
+
+interface IPREDEFINED_RANGES {
+  '0 to 9': CharRange
+  'a to z': CharRange
+  'A to Z': CharRange
+  '# to /': CharRange
+}
+
+export const PREDEFINED_RANGES: IPREDEFINED_RANGES = {
+  '0 to 9': ['0', '9'],
+  'a to z': ['a', 'z'],
+  'A to Z': ['A', 'Z'],
+  '# to /': ['#', '/']
+}
+
+interface IREVERSED_PREDEFINED_RANGES {
+  '9 to 0': CharRange
+  'z to a': CharRange
+  'Z to A': CharRange
+  '/ to #': CharRange
+}
+
+export const REVERSED_PREDEFINED_RANGES: IREVERSED_PREDEFINED_RANGES = (
+  globalThis.Object.fromEntries(
+    globalThis.Object
+      .entries(PREDEFINED_RANGES)
+      .map(
+        ([rangeTitle, charRange]: [string, CharRange]): [string, CharRange] => (
+          (rangeTitles: string[]): [string, CharRange] => [
+            `${rangeTitles[2]} ${rangeTitles[1]} ${rangeTitles[0]}`,
+            [...charRange].reverse() as CharRange
+          ]
+        )(rangeTitle.split(' '))
+      )
+  ) as unknown as IREVERSED_PREDEFINED_RANGES
+)
+
+export function rangeToCodeRange (range: Range): CodeRange {
+  const codeRange: number[] = []
+
+  if (typeof range[0] !== 'string') {
+    codeRange.push(...(range as CodeRange))
+  } else {
+    codeRange.push(
+      Number(
+        String.prototype.codePointAt.call(range[0], 0)
+      ),
+      Number(
+        String.prototype.codePointAt.call(range[1], 0)
+      )
+    )
+  }
+
+  return codeRange as CodeRange
+}
+
+export interface ReverseCharIDOptions {
+  ranges?: Range[]
+  lastID?: string
+}
+
+export class ReverseCharID {
+  #codeRanges: CodeRange[] = [PREDEFINED_RANGES['0 to 9']].map(rangeToCodeRange)
+  #lastID: string = ''
+
+  constructor ({ ranges, lastID }: ReverseCharIDOptions = {}) {
+    if (ranges instanceof Array) this.#codeRanges = ranges.map(rangeToCodeRange)
+    if (typeof lastID === 'string') this.#lastID = lastID
+  }
+
+  toValue (): string {
+    this.#lastID = generateNextID({
+      codeRanges: this.#codeRanges,
+      lastID: this.#lastID
+    })
+
+    return this.#lastID
+  }
+
+  [Symbol.toPrimitive]: () => string = this.toValue
+
+  toString: () => string = this.toValue
+}
