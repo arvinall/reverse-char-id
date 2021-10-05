188b23e85fe63f5c4e76a1558d54298f7bc101dd
Arvinall <arvinall021@gmail.com>
2021-10-05 09:14:18 +0330

    test(id): add getLastCodeRangesChar method test

--- src/tests/id.test.ts
+++ src/tests/id.test.ts
@@ -5,7 +5,10 @@ import {
 } from '@jest/globals'
 
 import { CodeRange } from '..'
-import { getFirstCodeRangesChar } from '../id'
+import {
+  getFirstCodeRangesChar,
+  getLastCodeRangesChar
+} from '../id'
 
 const {
   Number,
@@ -24,6 +27,20 @@ describe('Method', () => {
       getFirstCodeRangesChar(codeRanges)
     ).toBe(
       String.fromCodePoint(codeRanges[0][0])
+    )
+  })
+
+  test('getLastCodeRangesChar must return last character of codeRanges', () => {
+    const codeRanges: CodeRange[] = [
+      [Number('A'.codePointAt(0)), Number('Z'.codePointAt(0))],
+      [Number('0'.codePointAt(0)), Number('9'.codePointAt(0))],
+      [Number('a'.codePointAt(0)), Number('z'.codePointAt(0))]
+    ]
+
+    expect(
+      getLastCodeRangesChar(codeRanges)
+    ).toBe(
+      String.fromCodePoint(codeRanges[codeRanges.length - 1][1])
     )
   })
 })
